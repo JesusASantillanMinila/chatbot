37b3f7395523d205895dfca1f2301c618a012d99
JesusASantillanMinila <alosantillan@gmail.com>
2025-12-17 21:51:40 +0000

    solving library updates

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,12 +1,12 @@
 import streamlit as st
 from PyPDF2 import PdfReader
-# from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_google_genai import GoogleGenerativeAIEmbeddings
 from langchain_community.vectorstores import FAISS
 from langchain_google_genai import ChatGoogleGenerativeAI
+
 from langchain.chains.question_answering import load_qa_chain
-from langchain.prompts import PromptTemplate
+from langchain_core.prompts import PromptTemplate
 import google.generativeai as genai
 from google.oauth2 import service_account
 from googleapiclient.discovery import build
